b05ea3b084b4ca28858f0f1c6a410db74dd53cc8
Qwage <quinnj@ameritech.net>
2023-11-12 22:19:12 -0800

    update sensor readout

--- pi_scripts/sensor_readout.py
+++ pi_scripts/sensor_readout.py
@@ -6,42 +6,33 @@ import board
 import adafruit_dht
 import RPi.GPIO as gpio
 
-dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
-print(dhtDevice)
-for i in range(4):
+def get_temperature_and_humidity(device: adafruit_dht):
     try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
+        temperature_c = device.temperature
         temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just             #        keep going
-        print(error.args[0])
+        humidity = device.humidity
+        return temperature_f, humidity
+    except RuntimeError as _:
+        return None, None, None
+    except Exception as _:
+        device.exit()
+        return False
+
+# The actual POST request function
+def post_data(temperature, humidity):
+    url = 'http://your-django-app.com/api/sensor-update/'
+    headers = {'Content-Type': 'application/json'}  # Set the content type to application/json
+    data = json.dumps({'temperature': temperature, 'humidity': humidity})
+    response = requests.post(url, headers=headers, data=data)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+    for i in range(4):
+        temperature_f, humidity = get_temperature_and_humidity(dhtDevice)
+        print(f'Temperature: {temperature_f}\nHumidity: {humidity}\n\n')
         time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
 
-    time.sleep(2.0)
-
-gpio.cleanup()
-
-#
-# # The actual POST request function
-# def post_data(temperature, humidity):
-#     url = 'http://your-django-app.com/api/sensor-update/'
-#     headers = {'Content-Type': 'application/json'}  # Set the content type to application/json
-#     data = json.dumps({'temperature': temperature, 'humidity': humidity})
-#     response = requests.post(url, headers=headers, data=data)
-#     print(response.text)
-#
-#
-# if __name__ == '__main__':
-#     temperature, humidity = read_temperature_and_humidity()
-#     post_data(temperature, humidity)
+    # post_data(temperature, humidity)
+    gpio.cleanup()
