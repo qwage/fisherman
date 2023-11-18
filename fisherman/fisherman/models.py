4b0dbd3dc94091af3eedad053694fb39f043dfb8
Qwage <quinnj@ameritech.net>
2023-11-17 17:08:56 -0800

    updated style for reef background

--- fisherman/fisherman/models.py
+++ fisherman/fisherman/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class SensorModel(models.Model):
-    created = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)  # use auto_now_add for the creation timestamp
     temperature = models.FloatField()
 
     def __str__(self):
         return f"Sensor reading at {self.id} is {self.temperature} degrees"
+
+    class Meta:  # 'Meta' should be capitalized
+        db_table = 'sensor_data'
